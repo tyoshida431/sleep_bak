c46d9ac8d7856cfd32b21819a089581cae434a96
aoshima256 <aoshima256@gmail.com>
2026-08-26 19:00:04 +0900

    it's change function name.

--- app/CorsManager.go
+++ app/CorsManager.go
@@ -17,17 +17,17 @@ type Config struct{
   APICorsAllowOrigins []URL `json:"APICorsAllowOrigins"`
 }
 
-func loadConfig() ([]string, error){
+func loadAllowOrigins()([]string, error){
   var ret []string
   file,err:=os.Open("config.json")
   if err!=nil{
-    log.Fatal("loadConfig os.Open err:",err)
+    log.Fatal("loadConfig os.Open Error:",err)
     return nil,err
   }
   defer file.Close()
   jsonData,err:=ioutil.ReadAll(file)
   if err!=nil{
-    log.Fatal("ioutil.ReadAll err:",err)
+    log.Fatal("ioutil.ReadAll Error:",err)
     return nil,err
   }
   var conf Config
@@ -39,18 +39,13 @@ func loadConfig() ([]string, error){
 }
 
 func corsMiddleware()(gin.HandlerFunc,error){
-  var err error
   var allowOrigins []string
 
-  confList,err:=loadConfig()
+  allowOrigins,err:=loadAllowOrigins()
   if err!=nil{
     log.Fatal("load CorsOrigins Error:",err)
     return nil,err
   }
-  for _,conf:=range confList{
-    allowOrigins=append(allowOrigins,conf)
-  }
-  
   config:=cors.DefaultConfig()
   config.AllowOrigins=allowOrigins
   config.AllowHeaders=[]string{"Origin","Content-Type","Accept","Access-Control-Allow-Headers","X-CSRF-Token","Access-Control-Allow-Origin",}
