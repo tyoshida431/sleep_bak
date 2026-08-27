dd02f0445582a3da63a076ceec818961ba980d63
aoshima256 <aoshima256@gmail.com>
2026-08-27 10:21:37 +0900

    it's add sleep dir.

--- app/main.go
+++ app/main.go
@@ -8,21 +8,28 @@ import (
 
 func main() {
 	var err error
-	// Ginのルーターを作成
 	router := gin.Default()
 	cors, err := getCorsConfig()
 	if err == nil {
 		router.Use(cors)
 	}
-
-	// ルートエンドポイントを定義し、Hello Worldを返す
+	// TODO : Sleepモジュールを作って呼びます。
+	// TODO : クエリの取得からです。
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello World",
 		})
 	})
-
-	// ポート7070でサーバーを起動
+	router.GET("/sleep", func(c *gin.Context) {
+		month := c.Query("month")
+		log.Println(month)
+		//data, err := getSleep()
+		//if err == nil {
+		//	c.JSON(200, data)
+		//} else {
+		//	c.JSON(500, gin.H{"message": "サーバー内部でエラーが発生しました"})
+		//}
+	})
 	err = router.Run(":7070")
 	if err != nil {
 		log.Fatal("Cannot Run Server Error:", err)
